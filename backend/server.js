b5439a36e51378022085d0e4ba2d919988859599
Nadhifa Hassan <nadhifabarakat@gmail.com>
2026-04-30 20:58:02 +0200

    fix server

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,6 @@ import cors from "cors";
 import "dotenv/config";
 import path from "path";
 import { fileURLToPath } from "url";
-
 import skincareRoutes from "./routes/skincareRoutes.js";
 import offersRoutes from "./routes/offersRoutes.js";
 import laserRoutes from "./routes/laserRoutes.js";
@@ -13,12 +12,10 @@ import { login } from "./controllers/authControllers.js";
 import connectDB from "./config/db.js";
 
 const app = express();
-
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 app.use(express.json());
-
 app.use(
   cors({
     origin: "https://nadra-kr80.onrender.com",
@@ -34,14 +31,13 @@ app.use("/laser", laserRoutes);
 app.use("/contact", contactRoutes);
 app.use("/auth", authRoutes);
 app.post("/login", login);
-
 app.use("/images", express.static(path.join(__dirname, "public/images")));
-
 app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "../frontend/dist", "index.html"));
 });
+
 connectDB();
 
 const PORT = process.env.PORT || 3000;
