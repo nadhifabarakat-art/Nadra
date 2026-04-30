4dbad38d9707085598d98bfde9bcb2c92ed38fc2
Nadhifa Hassan <nadhifabarakat@gmail.com>
2026-04-30 17:14:33 +0200

    add login

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import cors from "cors";
 import "dotenv/config";
 import path from "path";
 import { fileURLToPath } from "url";
+
 import skincareRoutes from "./routes/skincareRoutes.js";
 import offersRoutes from "./routes/offersRoutes.js";
 import laserRoutes from "./routes/laserRoutes.js";
@@ -12,30 +13,36 @@ import { login } from "./controllers/authControllers.js";
 import connectDB from "./config/db.js";
 
 const app = express();
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 app.use(express.json());
+
 app.use(
   cors({
-    origin: process.env.NODE_ENV === "production" ? "*" : "*",
+    origin: "https://nadra-kr80.onrender.com",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
   }),
 );
+
 app.use("/skincare", skincareRoutes);
 app.use("/offers", offersRoutes);
 app.use("/laser", laserRoutes);
 app.use("/contact", contactRoutes);
 app.use("/auth", authRoutes);
 app.post("/login", login);
+
 app.use("/images", express.static(path.join(__dirname, "public/images")));
+
 app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-app.get(/^(?!\/api).*/, (req, res) => {
+app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
 });
+
 connectDB();
 
 const PORT = process.env.PORT || 3000;
--- frontend/src/Api.jsx
+++ frontend/src/Api.jsx
@@ -2,7 +2,6 @@ import axios from "axios";
 
 const api = axios.create({
   baseURL: "https://nadra-kr80.onrender.com",
-  withCredentials: true,
 });
 
 export default api;
